5708d8974fcabe64e431f360e222d9206ebba5c7
Aareschluchtje <remcosannen@gmail.com>
2016-05-13 15:23:51 +0200

    Added multiple window support

--- HolographDemo/HolographDemo/Main.cpp
+++ HolographDemo/HolographDemo/Main.cpp
@@ -9,23 +9,26 @@
 /*				Local Variable                                             */
 /*-------------------------------------------------------------------------*/
 
-	float width = 800;
-	float height = 600;
+	float Holographicwidth = 600;
+	float Holographicheight = 600;
 	float rotateX, rotateY, rotateZ = 0.0f;
 	double trX, trY = 0;
 	bool perspectiveFlag = true;
 	Cube c1, c2, c3, c4;
-	GLenum mode = GL_LINE;
+	GLenum mode = GL_FILL;
+	GLint holographic;
+	GLint second;
 
 /*-------------------------------------------------------------------------*/
 /*				Function Prototyping                                       */
 /*-------------------------------------------------------------------------*/
 
 	void Idle(void);
-	void SetupWindow(void);
-	void PaintComponent(void);
+	void HolographicSetupWindow(void);
+	void HolographicPaintComponent(void);
+	void SecondPaintComponent(void);
 	
-	void ReshapeWindow(int, int);
+	void HolographicReshapeWindow(int, int);
 
 	void KeyEvent(unsigned char, int, int);
 
@@ -41,22 +44,30 @@
 	
 	void Idle(void)
 	{
-		rotateY += 0.01f;
-		rotateX += 0.01f;
+		rotateY += 0.05f;
+		rotateX += 0.05f;
 		glutPostRedisplay();
+		if (glutGetWindow() != holographic)
+		{
+			glutSetWindow(holographic);
+		}
+		else
+		{
+			glutSetWindow(second);
+		}
 	}
 
-	void SetupWindow(void)
+	void HolographicSetupWindow(void)
 	{
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 		glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
 
-		glViewport(0, 0, width, height);
+		glViewport(0, 0, Holographicwidth, Holographicheight);
 
 		glMatrixMode(GL_PROJECTION);
 		glLoadIdentity();
 
-		gluPerspective(90, (width/height), 0.1f, 100);
+		gluPerspective(90, (Holographicwidth/Holographicwidth), 0.1f, 100);
 
 		glMatrixMode(GL_MODELVIEW);
 		glLoadIdentity();
@@ -66,54 +77,37 @@
 			0, 1, 0);
 	}
 
-	void PaintComponent(void)
+	void HolographicPaintComponent(void)
 	{
 		// Reset window
-		SetupWindow();
+		HolographicSetupWindow();
 
 		glPolygonMode(GL_FRONT_AND_BACK, mode);
 		
 		// Cube 1
 		glPushMatrix();
-			glTranslated(0, 2, 0);
 			glRotatef(rotateY, 0.0, 1.0, 0.0);
 			glRotatef(rotateX, 1.0, 0.0, 0.0);
 			c1.draw();
 		glPopMatrix();
 
+		glFlush();
+		glutSwapBuffers();
+	}
 
-		// Cube 2
-		glPushMatrix();
-			glTranslated(2, 0, 0);
-			glRotatef(rotateY, 0.0, 1.0, 0.0);
-			glRotatef(rotateX, 1.0, 0.0, 0.0);
-			c2.draw();
-		glPopMatrix();
-
-		// Cube 3
-		glPushMatrix();
-			glTranslated(0, -2, 0);
-			glRotatef(rotateY, 0.0, 1.0, 0.0);
-			glRotatef(rotateX, 1.0, 0.0, 0.0);
-			c3.draw();
-		glPopMatrix();
-
-		// Cube 4
-		glPushMatrix();
-			glTranslated(-2, 0, 0);
-			glRotatef(rotateY, 0.0, 1.0, 0.0);
-			glRotatef(rotateX, 1.0, 0.0, 0.0);
-			c4.draw();
-		glPopMatrix();
+	void SecondPaintComponent(void)
+	{
+		// Reset window
+		glPolygonMode(GL_FRONT_AND_BACK, mode);
 
 		glFlush();
 		glutSwapBuffers();
 	}
 	
-	void ReshapeWindow(int w, int h)
+	void HolographicReshapeWindow(int w, int h)
 	{
-		width = w;
-		height = h;
+		Holographicwidth = w;
+		Holographicheight = h;
 	}
 
 
@@ -137,16 +131,19 @@
 int main(int argc, char *argv[])
 {
 	glutInitDisplayMode(GLUT_RGB | GLUT_DOUBLE | GLUT_DEPTH);
-	glutInitWindowSize(int(width), int(height));
+	glutInitWindowSize(int(Holographicwidth), int(Holographicheight));
 	glutInit(&argc, argv);
-	glutCreateWindow("3D Objects");
-		
-	glEnable(GL_DEPTH_TEST);
-
+	holographic = glutCreateWindow("Hologram");
+	glutDisplayFunc(HolographicPaintComponent);
 	glutIdleFunc(Idle);
-	glutDisplayFunc(PaintComponent);
-	glutReshapeFunc(ReshapeWindow);
-
+	glutReshapeFunc(HolographicReshapeWindow);
+	glutKeyboardFunc(KeyEvent);
+	glutInitWindowSize(1920, 1080);
+	second = glutCreateWindow("2D Graphics");
+	glEnable(GL_DEPTH_TEST);
+	glutIdleFunc(Idle);
+	glutDisplayFunc(SecondPaintComponent);
+	//glutReshapeFunc(ReshapeWindow);
 	glutKeyboardFunc(KeyEvent);
 	glutMainLoop();
 	return 0;
