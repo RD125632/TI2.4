63c00a24ec7a028d89e88bb51d8239831f3d6e48
Aareschluchtje <remcosannen@gmail.com>
2016-06-02 15:10:29 +0200

    added recipes

--- HolographDemo/HolographDemo/Recipe.h
+++ HolographDemo/HolographDemo/Recipe.h
@@ -1,1 +1,13 @@
 #pragma once
+
+#include "Ingredient.h"
+#include <vector>
+
+class Recipe
+{
+public:
+	std::vector<Ingredient> ingredients;
+	std::string symptom;
+	std::string name;
+	int id;
+};
