f0a2ae0d6359a8fbfa7b95129a8b291713a977d7
Kees Zijlmans <kpzijlma@avans.nl>
2016-06-30 12:17:07 +0200

    text attempt

--- HolographDemo/HolographDemo/StoryScreen.cpp
+++ HolographDemo/HolographDemo/StoryScreen.cpp
@@ -2,23 +2,66 @@
 #include "GlobalCollector.h"
 #include "Ingredient.h"
 #include <iostream>
+GLfloat UpwardsScrollVelocity = -10.0;
+float view = 20.0;
 
+string quote[6];
+int numberOfQuotes = 0, i;
 StoryScreen::StoryScreen() : Screen()
 {
 	
 	screenToDraw = &StoryScreen::drawIntroScreen;
+	
 }
 void StoryScreen::drawIntroScreen()
 {
-	glPushMatrix();
-	glMatrixMode(GL_PROJECTION);
-	glLoadIdentity();
-	glOrtho(0, 1000, 0, 1000, 0, 1);
-	glMatrixMode(GL_MODELVIEW);
-	glRasterPos2i(100, 120);
-	glColor4f(0.0f, 0.0f, 1.0f, 1.0f);
-	glutBitmapCharacter(GLUT_BITMAP_HELVETICA_18, '1');
+	//glPushMatrix();
+	 glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+	//glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+	//glOrtho(0, 4000, -50, 2000, 5, -5);
+	//glMatrixMode(GL_MODELVIEW);
+	//glEnable(GL_COLOR_MATERIAL);
+	//glColor4f(1.0f, 0.0f, 0.0f, 1.0f);
+	//string speedtext = "test test test test test test test";
+	//glLoadIdentity();
+	//glTranslatef(0, 140, 0);
+	//for (int i = 0; i < speedtext.length(); i++)
+	//{
+	//	glutStrokeCharacter(GLUT_STROKE_ROMAN, speedtext[i]);
+	//}
+	 glMatrixMode(GL_PROJECTION);
+	 glPushMatrix();
+	 glLoadIdentity();
+	 glEnable(GL_COLOR_MATERIAL);
+	 glColor4f(1.0f, 1.0f, 1.0f, 1.0f);
+	 glOrtho(-2000, 2000, -2000, 2000, 5, -5);
+	 glMatrixMode(GL_MODELVIEW);
+	int l, lenghOfQuote, i;
+	for (l = 0;l<numberOfQuotes;l++)
+	{
+		lenghOfQuote = quote[l].length();
+		//glPushMatrix();
+		//glTranslated(10, 100, 10);
+		glTranslatef(-(lenghOfQuote * 37), -(l * 200), 0.0);
+		//glTranslatef(0, 1800, 0);
+		for (i = 0; i < lenghOfQuote; i++)
+		{
+			glColor3f((UpwardsScrollVelocity / 10) + 300 + (l * 10), (UpwardsScrollVelocity / 10) + 300 + (l * 10), 0.0);
+			glutStrokeCharacter(GLUT_STROKE_ROMAN, quote[l][i]);
+		}
+		glPopMatrix();
+	}
+	/*string scoretext = "Score: " + std::to_string(score);
+	for (int i = 0; i < scoretext.length(); i++)
+	{
+		glutStrokeCharacter(GLUT_STROKE_ROMAN, scoretext[i]);
+	}*/
+	
+
+
 	glPopMatrix();
+	glFlush();
+	glutSwapBuffers();
 }
 void StoryScreen::drawGameScreen()
 {
@@ -78,6 +121,11 @@ int StoryScreen::Setup(int windowWidth, int windowHeight)
 		0, 20, 20,
 		0, 1, 0);
 	std::cout << GlobalCollector::Instance()->camera.currentlocation[0] << GlobalCollector::Instance()->camera.currentlocation[1] << GlobalCollector::Instance()->camera.currentlocation[2] << endl;
-
+	quote[0] = "Luke, I am your father!.";
+	quote[1]=	"Obi-Wan has taught you well. ";
+	quote[2]= "The force is strong with this one. ";
+	quote[3]= "Alert all commands. Calculate every possible destination along their last known trajectory. ";
+	quote[4]= "The force is with you, young Skywalker, but you are not a Jedi yet.";
+	numberOfQuotes = 5;
 	return 1;
 }
