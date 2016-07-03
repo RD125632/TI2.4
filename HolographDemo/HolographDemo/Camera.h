690a087e321f1dd8bbad631f303f3ae6eda5e597
Aareschluchtje <remcosannen@gmail.com>
2016-07-04 00:45:41 +0200

    changed camera

--- HolographDemo/HolographDemo/Camera.h
+++ HolographDemo/HolographDemo/Camera.h
@@ -6,7 +6,7 @@ class Camera
 public:
 	std::array<double, 6> locations1{ 20, -10, 10, 20, -9, 20 };
 	std::array<double, 6> locations2{ 0, 20, 15, 0, 20, 20 };
-	std::array<double, 6> locations3{ -15 , 0, 25, -16, -5, 40 };
+	std::array<double, 6> locations3{ -15 , 3, 25, -16, -5, 40 };
 	std::array<double, 6> currentlocation = locations1;
 	std::array<double, 6> targetlocation = locations1;
 	std::array<double, 6> steps{ 0,0,0,0,0,0 };
